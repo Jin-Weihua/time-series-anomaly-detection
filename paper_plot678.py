324c08afeb3e68f611cac66dfd4624f30f3cf1cf
wh.jin <wh.jin@hotmail.com>
2019-05-07 09:57:48 +0800

    d

---
+++ paper_plot678.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   data_standardize.py
+@Time    :   2019/01/17 19:58:52
+@Author  :   靳卫华 
+@Version :   1.0
+@Contact :   wh.jin@hotmail.com
+@Desc    :   None
+'''
+
+# access to system parameters
+import sys
+print("Python version: {}".format(sys.version))
+
+# collection of functions for data processing and analysis
+# modeled after R dataframes with SQL like features
+import pandas as pd
+print("pandas version: {}".format(pd.__version__))
+
+# collection of functions for scientific and publication-ready visualization
+import matplotlib
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdate
+from matplotlib.font_manager import FontProperties
+print("matplotlib version: {}".format(matplotlib.__version__))
+
+# foundational package for scientific computing
+import numpy as np
+print("NumPy version: {}".format(np.__version__))
+from sklearn.preprocessing import MinMaxScaler
+# ignore warnings
+# import warnings
+# warnings.filterwarnings('ignore')
+print('-' * 25)
+
+font = FontProperties(fname='/Library/Fonts/Songti.ttc')
+mpl.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+mpl.rcParams['axes.titlesize'] = 6
+mpl.rc('xtick', labelsize=6)  # 设置坐标轴刻度显示大小
+mpl.rc('ytick', labelsize=6)
+font_size = 1
+
+# import data
+# 列名字有中文的时候，encoding='utf-8',不然会出错
+# index_col设置属性列，parse_dates设置是否解析拥有时间值的列
+dateparser= lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
+data_basic = pd.read_csv(
+    'result/autoencoder2/autoencoder2-prd.csv',
+    sep=',',
+    index_col=0,
+    encoding='utf-8',
+    parse_dates=True,
+    date_parser=dateparser)
+
+dateparser = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
+data_SAE = pd.read_csv(
+    'result/autoencoder1/autoencoder1-prd.csv',
+    sep=',',
+    index_col=0,
+    encoding='utf-8',
+    parse_dates=True,
+    date_parser=dateparser)
+# DAE和DEAS反过来展示，哎
+dateparser = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
+data_DAE = pd.read_csv(
+    'result/autoencoder6/autoencoder6-prd-180.csv',
+    sep=',',
+    index_col=0,
+    encoding='utf-8',
+    parse_dates=True,
+    date_parser=dateparser)
+
+dateparser = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
+data_DAES = pd.read_csv(
+    'result/autoencoder7/autoencoder7-prd7-249.csv',
+    sep=',',
+    index_col=0,
+    encoding='utf-8',
+    parse_dates=True,
+    date_parser=dateparser)
+
+# column1='VNZ4A组蓄电池BEA信号'
+# ylabel = 'Normalized BEA value'
+column1='INZ14_ABCR1输入电流'
+ylabel = 'Normalized BCR input current'
+# column3='INZ6_-Y太阳电池阵电流'
+# ylabel = 'Normalized BCR input current'
+# column1='INA4_A电池组充电电流'
+# ylabel = 'Normalized battery set charge current'
+fig1 = plt.figure(figsize=(6, 5))
+
+ax1 = fig1.add_subplot(1, 4, 1)
+ax1.spines['top'].set_visible(False)  #去掉上边框
+ax1.spines['right'].set_visible(False) #去掉右边框
+ax1.plot(data_basic[1000:7000].loc[:, column1], '-', color='green', linewidth=1.0)
+# ax.plot(date_scaler[1000:7000].loc[:, column2], '-', color='C7', linewidth=1.0,label='BCR input current')
+# ax.legend(loc='right',fontsize='xx-small')
+# ax.xaxis.set_major_formatter(mdate.DateFormatter('%Y-%m-%d %H:%M:%S'))
+ax1.xaxis.set_major_formatter(mdate.DateFormatter('%H:%M:%S'))
+ax1.xaxis.set_tick_params(rotation=30)
+for label in ax1.get_xticklabels():
+    label.set_visible(False)
+for label in ax1.get_xticklabels()[::2]:
+    label.set_visible(True)
+# ax.set_title('bus current',fontsize=12,fontweight='bold')
+plt.xlabel('Time(h)',fontsize=8,fontweight='bold')
+plt.ylabel(ylabel,fontsize=8,fontweight='bold')
+ax1.set_title('Basic-T',fontsize=10)
+# plt.legend()
+plt.grid(linestyle = "--", alpha=0.4)
+
+ax2 = fig1.add_subplot(1, 4, 2, sharey=ax1)
+ax2.spines['top'].set_visible(False)  #去掉上边框
+ax2.spines['right'].set_visible(False) #去掉右边框
+ax2.spines['left'].set_visible(False) #去掉右边框
+ax2.plot(data_SAE[1000:7000].loc[:, column1], '-', color='green', linewidth=1.0)
+# ax.legend(loc='right',fontsize='xx-small')
+ax2.xaxis.set_major_formatter(mdate.DateFormatter('%H:%M:%S'))
+ax2.xaxis.set_tick_params(rotation=30)
+for label in ax2.get_xticklabels():
+    label.set_visible(False)
+for label in ax2.get_xticklabels()[::2]:
+    label.set_visible(True)
+for label in ax2.get_yticklabels():
+    label.set_visible(False)
+# ax.set_title('bus current',fontsize=12,fontweight='bold')
+plt.xlabel('Time(h)', fontsize=8,fontweight='bold')
+ax2.set_title('SAE-T',fontsize=10)
+# plt.ylabel(ylabel,fontsize=8,fontweight='bold')
+# plt.legend()
+plt.grid(linestyle = "--", alpha=0.4)
+
+
+ax3 = fig1.add_subplot(1, 4, 3, sharey=ax1)
+ax3.spines['top'].set_visible(False)  #去掉上边框
+ax3.spines['right'].set_visible(False) #去掉右边框
+ax3.spines['left'].set_visible(False) #去掉右边
+ax3.plot(data_DAE[1000:7000].loc[:, column1], '-', color='green', linewidth=1.0)
+# ax.plot(date_scaler[1000:7000].loc[:, column3], '-', color='C7', linewidth=1.0,label='Solar cell array current')
+# ax.legend(loc='right',fontsize='xx-small')
+ax3.xaxis.set_major_formatter(mdate.DateFormatter('%H:%M:%S'))
+ax3.xaxis.set_tick_params(rotation=30)
+for label in ax3.get_xticklabels():
+    label.set_visible(False)
+for label in ax3.get_xticklabels()[::2]:
+    label.set_visible(True)
+for label in ax3.get_yticklabels():
+    label.set_visible(False)
+    # ax.set_title('bus current',fontsize=12,fontweight='bold')
+plt.xlabel('Time(h)', fontsize=8,fontweight='bold')
+ax3.set_title('DAE-T',fontsize=10)
+# plt.ylabel(ylabel,fontsize=8,fontweight='bold')
+# plt.legend()
+plt.grid(linestyle = "--", alpha=0.4)
+
+ax4 = fig1.add_subplot(1, 4, 4, sharey=ax1)
+ax4.spines['top'].set_visible(False)  #去掉上边框
+ax4.spines['right'].set_visible(False) #去掉右边框
+ax4.spines['left'].set_visible(False) #去掉右边框
+ax4.plot(data_DAES[1000:7000].loc[:, column1], '-', color='green', linewidth=1.0)
+# ax.legend(loc='right',fontsize='xx-small')
+ax4.xaxis.set_major_formatter(mdate.DateFormatter('%H:%M:%S'))
+ax4.xaxis.set_tick_params(rotation=30)
+for label in ax4.get_xticklabels():
+    label.set_visible(False)
+for label in ax4.get_xticklabels()[::2]:
+    label.set_visible(True)
+for label in ax4.get_yticklabels():
+    label.set_visible(False)
+# ax.set_title('bus current',fontsize=12,fontweight='bold')
+plt.xlabel('Time(h)', fontsize=8,fontweight='bold')
+ax4.set_title('DAE-S',fontsize=10)
+# plt.ylabel(ylabel,fontsize=8,fontweight='bold')
+# plt.legend()
+plt.grid(linestyle = "--", alpha=0.4)
+
+plt.subplots_adjust(left=0.10, bottom=0.15, right=0.95, top=0.90, hspace=0.4, wspace=0.1)
+# left=0.05, bottom=0.03, right=0.95, top=0.97 分别代表到画布的左侧和底部的距离占整幅图宽和高的比例
+# plt.show()
+plt.savefig('result/7.svg',format='svg')
+plt.savefig('result/7.png',format='png')
---
+++ paper_plot9.py
@@ -0,0 +1,245 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   data_standardize.py
+@Time    :   2019/01/17 19:58:52
+@Author  :   靳卫华 
+@Version :   1.0
+@Contact :   wh.jin@hotmail.com
+@Desc    :   None
+'''
+
+# access to system parameters
+import sys
+print("Python version: {}".format(sys.version))
+
+import plotly
+plotly.tools.set_credentials_file(username='wh.jin', api_key='DADs4nY8rboe9LuL6Iuz')
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+# collection of functions for data processing and analysis
+# modeled after R dataframes with SQL like features
+import pandas as pd
+print("pandas version: {}".format(pd.__version__))
+
+# collection of functions for scientific and publication-ready visualization
+import matplotlib
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdate
+from matplotlib.font_manager import FontProperties
+print("matplotlib version: {}".format(matplotlib.__version__))
+
+# foundational package for scientific computing
+import numpy as np
+print("NumPy version: {}".format(np.__version__))
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.cluster import KMeans
+# ignore warnings
+# import warnings
+# warnings.filterwarnings('ignore')
+print('-' * 25)
+
+font = FontProperties(fname='/Library/Fonts/Songti.ttc')
+mpl.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+mpl.rcParams['axes.titlesize'] = 6
+mpl.rc('xtick', labelsize=6)  # 设置坐标轴刻度显示大小
+mpl.rc('ytick', labelsize=6)
+font_size = 1
+
+# import data
+# 列名字有中文的时候，encoding='utf-8',不然会出错
+# index_col设置属性列，parse_dates设置是否解析拥有时间值的列
+dateparser= lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
+data_rolling = pd.read_csv(
+    'data/data_rolling.csv',
+    sep=',',
+    index_col=0,
+    encoding='utf-8',
+    parse_dates=True,
+    date_parser=dateparser)
+dataset_rolling = data_rolling.as_matrix()
+
+dateparser= lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
+data_basic = pd.read_csv(
+    'result/autoencoder2/autoencoder2-prd.csv',
+    sep=',',
+    index_col=0,
+    encoding='utf-8',
+    parse_dates=True,
+    date_parser=dateparser)
+dataset_basic = data_basic.as_matrix()
+index = data_basic.index
+dist = np.linalg.norm(dataset_basic - dataset_rolling, axis=-1).reshape(-1,1)
+data_dist = pd.DataFrame(dist, index=index, columns=['norm']).plot.scatter()
+
+clf = KMeans(n_clusters=2)
+data_dist['label'] = clf.fit_predict(data_dist[['norm']])
+column = ['INZ14_ABCR1输入电流']
+data_basic1 = data_basic.loc[:,column].iloc[0:10000]
+data_dist1 = data_dist.loc[:,['label']].iloc[0:10000]
+data = []
+data.append(go.Scatter(x=data_dist1.index, y=data_dist1['label'], mode='markers', name='label'))
+column = 'INZ14_ABCR1输入电流'
+data.append(go.Scatter(x=data_basic1.index, y=data_basic1[column], mode='markers', name=column))
+data_dist2 = data_dist.iloc[0:10000]
+# plotly.offline.plot(data, filename='w.html', auto_open=True)
+ax = data_dist2[data_dist2['label']==0].plot.scatter(x='norm', y='label', s=50, color='white', edgecolor='black')
+data_dist2[data_dist2['label']==1].plot.scatter(x='norm', y='label', s=50, color='white', ax=ax, edgecolor='red')
+plt.scatter(clf.cluster_centers_.ravel(), [0.5]*len(clf.cluster_centers_), s=100, color='green', marker='*')
+
+numSamples=len(dist)
+centroids = clf.labels_
+print(centroids,type(centroids)) 
+print(clf.inertia_)
+mark = ['or', 'ob', 'og', 'ok', '^r', '+r', 'sr', 'dr', '<r', 'pr']
+#画出所有样例点 属于同一分类的绘制同样的颜色
+for i in range(numSamples):
+    #markIndex = int(clusterAssment[i, 0])
+    dist[i][0]
+    # dist[i][1]
+    clf.labels_[i]
+    mark[clf.labels_[i]]
+    plt.plot(dist[i][0], mark[clf.labels_[i]]) #mark[markIndex])
+    mark = ['Dr', 'Db', 'Dg', 'Dk', '^b', '+b', 'sb', 'db', '<b', 'pb']
+    # 画出质点，用特殊图型
+    centroids =clf.cluster_centers_
+    for i in range(2):
+        plt.plot(centroids[i][0], mark[i], markersize = 12)
+        #print centroids[i, 0], centroids[i, 1]
+plt.show()
+
+
+dateparser = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
+data_SAE = pd.read_csv(
+    'result/autoencoder1/autoencoder1-prd.csv',
+    sep=',',
+    index_col=0,
+    encoding='utf-8',
+    parse_dates=True,
+    date_parser=dateparser)
+dataset_SAE = data_SAE.as_matrix()
+
+
+# DAE和DEAS反过来展示，哎
+dateparser = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
+data_DAE = pd.read_csv(
+    'result/autoencoder6/autoencoder6-prd-180.csv',
+    sep=',',
+    index_col=0,
+    encoding='utf-8',
+    parse_dates=True,
+    date_parser=dateparser)
+dataset_DAE = data_DAE.as_matrix()
+
+dateparser = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
+data_DAES = pd.read_csv(
+    'result/autoencoder7/autoencoder7-prd7-249.csv',
+    sep=',',
+    index_col=0,
+    encoding='utf-8',
+    parse_dates=True,
+    date_parser=dateparser)
+dataset_DAES = data_DAES.as_matrix()
+
+# column1='VNZ4A组蓄电池BEA信号'
+# ylabel = 'Normalized BEA value'
+column1='INZ14_ABCR1输入电流'
+ylabel = 'Normalized BCR input current'
+# column3='INZ6_-Y太阳电池阵电流'
+# ylabel = 'Normalized BCR input current'
+# column1='INA4_A电池组充电电流'
+# ylabel = 'Normalized battery set charge current'
+fig1 = plt.figure(figsize=(6, 5))
+
+ax1 = fig1.add_subplot(1, 4, 1)
+ax1.spines['top'].set_visible(False)  #去掉上边框
+ax1.spines['right'].set_visible(False) #去掉右边框
+ax1.plot(data_basic[1000:7000].loc[:, column1], '-', color='green', linewidth=1.0)
+# ax.plot(date_scaler[1000:7000].loc[:, column2], '-', color='C7', linewidth=1.0,label='BCR input current')
+# ax.legend(loc='right',fontsize='xx-small')
+# ax.xaxis.set_major_formatter(mdate.DateFormatter('%Y-%m-%d %H:%M:%S'))
+ax1.xaxis.set_major_formatter(mdate.DateFormatter('%H:%M:%S'))
+ax1.xaxis.set_tick_params(rotation=30)
+for label in ax1.get_xticklabels():
+    label.set_visible(False)
+for label in ax1.get_xticklabels()[::2]:
+    label.set_visible(True)
+# ax.set_title('bus current',fontsize=12,fontweight='bold')
+plt.xlabel('Time(h)',fontsize=8,fontweight='bold')
+plt.ylabel(ylabel,fontsize=8,fontweight='bold')
+ax1.set_title('Basic-T',fontsize=10)
+# plt.legend()
+plt.grid(linestyle = "--", alpha=0.4)
+
+ax2 = fig1.add_subplot(1, 4, 2, sharey=ax1)
+ax2.spines['top'].set_visible(False)  #去掉上边框
+ax2.spines['right'].set_visible(False) #去掉右边框
+ax2.spines['left'].set_visible(False) #去掉右边框
+ax2.plot(data_SAE[1000:7000].loc[:, column1], '-', color='green', linewidth=1.0)
+# ax.legend(loc='right',fontsize='xx-small')
+ax2.xaxis.set_major_formatter(mdate.DateFormatter('%H:%M:%S'))
+ax2.xaxis.set_tick_params(rotation=30)
+for label in ax2.get_xticklabels():
+    label.set_visible(False)
+for label in ax2.get_xticklabels()[::2]:
+    label.set_visible(True)
+for label in ax2.get_yticklabels():
+    label.set_visible(False)
+# ax.set_title('bus current',fontsize=12,fontweight='bold')
+plt.xlabel('Time(h)', fontsize=8,fontweight='bold')
+ax2.set_title('SAE-T',fontsize=10)
+# plt.ylabel(ylabel,fontsize=8,fontweight='bold')
+# plt.legend()
+plt.grid(linestyle = "--", alpha=0.4)
+
+
+ax3 = fig1.add_subplot(1, 4, 3, sharey=ax1)
+ax3.spines['top'].set_visible(False)  #去掉上边框
+ax3.spines['right'].set_visible(False) #去掉右边框
+ax3.spines['left'].set_visible(False) #去掉右边
+ax3.plot(data_DAE[1000:7000].loc[:, column1], '-', color='green', linewidth=1.0)
+# ax.plot(date_scaler[1000:7000].loc[:, column3], '-', color='C7', linewidth=1.0,label='Solar cell array current')
+# ax.legend(loc='right',fontsize='xx-small')
+ax3.xaxis.set_major_formatter(mdate.DateFormatter('%H:%M:%S'))
+ax3.xaxis.set_tick_params(rotation=30)
+for label in ax3.get_xticklabels():
+    label.set_visible(False)
+for label in ax3.get_xticklabels()[::2]:
+    label.set_visible(True)
+for label in ax3.get_yticklabels():
+    label.set_visible(False)
+    # ax.set_title('bus current',fontsize=12,fontweight='bold')
+plt.xlabel('Time(h)', fontsize=8,fontweight='bold')
+ax3.set_title('DAE-T',fontsize=10)
+# plt.ylabel(ylabel,fontsize=8,fontweight='bold')
+# plt.legend()
+plt.grid(linestyle = "--", alpha=0.4)
+
+ax4 = fig1.add_subplot(1, 4, 4, sharey=ax1)
+ax4.spines['top'].set_visible(False)  #去掉上边框
+ax4.spines['right'].set_visible(False) #去掉右边框
+ax4.spines['left'].set_visible(False) #去掉右边框
+ax4.plot(data_DAES[1000:7000].loc[:, column1], '-', color='green', linewidth=1.0)
+# ax.legend(loc='right',fontsize='xx-small')
+ax4.xaxis.set_major_formatter(mdate.DateFormatter('%H:%M:%S'))
+ax4.xaxis.set_tick_params(rotation=30)
+for label in ax4.get_xticklabels():
+    label.set_visible(False)
+for label in ax4.get_xticklabels()[::2]:
+    label.set_visible(True)
+for label in ax4.get_yticklabels():
+    label.set_visible(False)
+# ax.set_title('bus current',fontsize=12,fontweight='bold')
+plt.xlabel('Time(h)', fontsize=8,fontweight='bold')
+ax4.set_title('DAE-S',fontsize=10)
+# plt.ylabel(ylabel,fontsize=8,fontweight='bold')
+# plt.legend()
+plt.grid(linestyle = "--", alpha=0.4)
+
+plt.subplots_adjust(left=0.10, bottom=0.15, right=0.95, top=0.90, hspace=0.4, wspace=0.1)
+# left=0.05, bottom=0.03, right=0.95, top=0.97 分别代表到画布的左侧和底部的距离占整幅图宽和高的比例
+# plt.show()
+plt.savefig('result/7.svg',format='svg')
+plt.savefig('result/7.png',format='png')
